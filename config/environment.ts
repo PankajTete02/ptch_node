0c5b5d0bac96162f0dd5a7397a9a10d1c82f020a
Pankaj Tete <pankaj.tete@cylsys.com>
2024-10-28 14:39:49 +0530

    env

---
+++ config/environment.ts
@@ -0,0 +1,5 @@
+export const DB_USER='pitchlabadmin';
+export const DB_PASSWORD='pitchlabCylsys@2';
+// export const DB_SERVER =;
+// export const DB_NAME = 'pitchlabdev';
+// export const DB_PORT="1433"
